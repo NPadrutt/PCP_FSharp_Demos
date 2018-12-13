5d51e3e33ebdc308e3e6aca2f7544a6d9ab28f00
Adrian Kauz <adrian.kauz@stud.hslu.ch>
2018-12-13 22:33:06 +0100

    Added simple Aktor and Async example

---
+++ PCP_FSharp_Scripts/AktorSimpleModel.fsx
@@ -0,0 +1,24 @@
+﻿(*
+  "rec":                Eine Funktion explizit als rekursiv Aufrufbar definieren, so dass deren Name auch innerhalb ihres Scopes sichtbar ist.
+                        Ansonsten kann sie sich nicht selbts wieder aufrufen.
+   "MailboxProcessor":  Dedizierte Message-Queue, welche auf einem eigenen Kontroll-Thread läuft. Jeder Thread kann dem Mailbox-Prozessor asynchron
+                        oder synchron eine Message übergeben. Diese wird dann via Aktor ausgewertet.
+*)
+
+open System
+
+let mailbox =
+    MailboxProcessor.Start(fun mb ->
+        let rec loop x =                                                // Definiere "loop" explizit als rekursive Funktion (Scope!)
+            async { let! msg = mb.Receive()                             // "Receive()" wartet hier auf eine neue Nachricht. Einstiegspunkt von aussen!
+                    let x = x + msg
+                    printfn "Running total: %i - new value %i" x msg
+                    return! loop x }                                    // "return!" führt einen neuen async-Workflow aus und wartet auf das Resultat
+        loop 0)                                                         // Hier erfolgt der erste Aufruf von "loop"
+
+mailbox.Post(1)
+mailbox.Post(2)
+mailbox.Post(3)
+mailbox.Post(4)
+mailbox.Post(5)
+mailbox.Post(6)
---
+++ PCP_FSharp_Scripts/AsyncExample.fsx
@@ -0,0 +1,23 @@
+﻿open System
+
+printfn "F# Async Example!"
+printfn "-----------------" 
+
+let simulatedJob id time =
+    let timestamp() = System.DateTime.Now.Ticks
+        
+    async {
+        printfn "Job %d start" id
+        let timestamp1 = timestamp()
+        do! Async.Sleep(time * 1000)                        // Wartet hier bis Sleep durchgelaufen ist. Ignoriere Rückgabewert
+        let timestamp2 = timestamp()
+        let timespan = System.TimeSpan(timestamp2 - timestamp1)
+        printfn "Job %d end %s" id (timespan.ToString("G"))
+    }
+
+// Stream Processing
+[ 1 .. 10]                                                  // Liste mit Nummern 1 bis 10
+|> List.mapi (fun index time -> simulatedJob index time)    // Lambda Expression -> Führe jeweils pro Element "simulatedJob" aus
+|> Async.Parallel                                           // Parallele Abarbeitung
+|> Async.RunSynchronously                                   // Starte alle miteinander
+|> ignore                                                   // Ignoriere Rückgabewert
